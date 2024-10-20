d6b60b37d50bb35353e486ad76ec9df6b52fbd0f
vrashabh-k <vvkam17@gmail.com>
2024-10-20 23:48:58 +0530

    addtocart-1 conti

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useNavigate} from "react-router-dom";
+import { Badge } from "react-bootstrap";
 export default function Navbar() {
 
 const navigate = useNavigate();
@@ -41,7 +42,8 @@ const handleLogout=()=>{
       :
       <div>
         <div className="btn bg-white text-success mx-2">
-        My Cart 
+        My Cart {" "}
+        <Badge pill bg="danger">2</Badge>
         </div>
       <div id="logoutbtn" className="btn bg-white text-danger mx-2" onClick={handleLogout}>
         Logout
